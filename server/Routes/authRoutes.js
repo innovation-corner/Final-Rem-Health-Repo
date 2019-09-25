6946bc2d025f5b47bd32cd050c68977e23e31ec5
Chinedu Ebinim <chinedu@verifi.ng>
2019-09-25 11:47:29 +0100

    add .env to .gitignore

--- server/Routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const auth = require("../Controllers/AuthController");
-
-router.post("/register", auth.register);
-router.post("/login", auth.login);
-// router.post("/view/:id", auth.resetPassword);
-
-module.exports = router;
--- server/Routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const user = require("../Controllers/UserController");
-
-router.get("/view", user.single);
-router.put("/edit/:id", user.edit);
-router.delete("/delete/:id", user.delete);
-
-module.exports = router;
