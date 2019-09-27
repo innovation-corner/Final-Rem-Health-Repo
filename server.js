8b94f2f1cc5ad49541a10a09ecc183cd4425ba6d
Chinedu Ebinim <chinedu@verifi.ng>
2019-09-27 07:43:10 +0100

    run build

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
+// const favicon = require("express-favicon");
+const path = require("path");
 
-app.use("/", express.static("build"));
+// app.use(favicon(__dirname + "/build/favicon.ico"));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+
+app.get("/*", function(req, res) {
+    console.log(req)
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("Server is running at:", PORT);
