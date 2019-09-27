8d489bc8e67304d5c83c0afcbff278cbacf87b49
root <enoch4real7@gmail.com>
2019-09-27 06:43:58 +0000

    build

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000
 
-app.use("/", express.static("build"));
+// const favicon = require("express-favicon");
+const path = require("path");
+
+// app.use(favicon(__dirname + "/build/favicon.ico"));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+
+app.get("/*", function(req, res) {
+    console.log(req)
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("Server is running at:", PORT);
