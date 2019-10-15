39cfdcd2fd8c6b9befebafdc3a4b498e4b25bcf7
Chinedu Ebinim <chinedu@verifi.ng>
2019-10-15 22:41:38 +0100

    fix immunization create error

--- server/Routes/immunizationRoutes.js
+++ server/Routes/immunizationRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const im = require("../Controllers/ImmunizationController");
 
-router.post("/add", im.create);
+router.post("/add/:id", im.create);
 router.get("/child/:id", im.getChildImmunization);
 router.put("/edit/:id", im.editSingle);
 router.get("/view/:id", im.listSingle);
