b4dfb452b91cfe80a93fa9970cf46e175de95b60
Chinedu Ebinim <chinedu@verifi.ng>
2019-10-15 21:47:04 +0100

    add immunizzation routes

--- server/Routes/index.js
+++ server/Routes/index.js
@@ -5,13 +5,15 @@ const user = require("./userRoutes");
 const hs = require("./hospitalRoutes");
 const sms = require("./smsRoutes");
 const hmo = require("./hmoRoutes");
-const disease = require('./diseaseRoutes')
+const disease = require('./diseaseRoutes');
+const im = require('./immunizationRoutes');
 
 module.exports = {
   ussd,
   user,
   info,
   hs,
+  im,
   disease,
   hmo,
   sms,
