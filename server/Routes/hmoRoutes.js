93b67be4d34a3c58f70da7d580dbe93c15c54d69
Chinedu Ebinim <chinedu@verifi.ng>
2019-10-15 08:52:57 +0100

    fix route

--- server/Routes/hmoRoutes.js
+++ server/Routes/hmoRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const hmo = require("../Controllers/HMOController");
 
-router.post("/add/", hmo.register);
 router.put("/edit/:id", hmo.edit);
 router.get("/view/:id", hmo.addChild);
 router.get("/view/all", hmo.viewAll);
+router.post("/add", hmo.register);
 
 module.exports = router;
