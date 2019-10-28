e23a04be741093d1e083eb5609c3ed227cfe59f6
Chinedu Ebinim <chinedu@verifi.ng>
2019-10-28 20:56:49 +0100

    add vaccine details

--- server/Routes/vaccineRoutes.js
+++ server/Routes/vaccineRoutes.js
@@ -5,5 +5,6 @@ const vaccine = require("../Controllers/VaccineController");
 router.post("/add", vaccine.create);
 router.post("/remove/:id", vaccine.delete);
 router.get("/get", vaccine.retrieve);
+router.get("/details", vaccine.vaccinedetails);
 
 module.exports = router;
