07f37e9de5cd22ae85233066dc10e18572baa189
Chinedu Ebinim <chinedu@verifi.ng>
2020-02-24 10:03:59 +0100

    fix crash

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ require("dotenv").config();
 
 // winston.log("info", "Hello World from Node.js!");
 
-console.log('hi', process.env.APPOPTICS_SERVICE_KEY);
+console.log('hi', process.env);
 require("./server/config/passport");
 const app = express();
 
